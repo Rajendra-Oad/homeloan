364e346ae55bcef0d36b10f224601aa7177d91ef
Rajendra-Oad <154015159+Rajendra-Oad@users.noreply.github.com>
2024-05-18 12:35:52 +0530

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,5 +13,34 @@ document.addEventListener('DOMContentLoaded', (event) => {
         });
     });
 });
+let btn = document.getElementById("topScroll");
+let navbar = document.getElementById("navbar");
+let prevScrollpos = window.pageYOffset;
+
+window.onscroll = () => {
+    let currentScrollPos = window.pageYOffset;
+
+    // Show or hide the scroll to top button
+    if (currentScrollPos > 725) {
+        btn.classList.add("show");
+    } else {
+        btn.classList.remove("show");
+    }
+
+    // Show or hide the navbar
+    if (prevScrollpos > currentScrollPos + 2) {
+        navbar.style.top = "0px"; // Show the navbar
+    } else if (prevScrollpos < currentScrollPos - 2) {
+        navbar.style.top = "-100px"; // Hide the navbar (adjust height if needed)
+    }
+
+    prevScrollpos = currentScrollPos; // Update previous scroll position
+};
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
+
 
 
